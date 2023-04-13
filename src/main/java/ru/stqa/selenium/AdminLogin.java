1aeb23a33585973c6d15b73fde26e4f045f736f9
Ellina Bessonova <ellystv11@mail.ru>
2023-04-13 18:22:42 +0300

    add second homework AdminLogin

---
+++ src/main/java/ru/stqa/selenium/AdminLogin.java
@@ -0,0 +1,38 @@
+package ru.stqa.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static java.time.Duration.ofSeconds;
+
+public class AdminLogin {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(ofSeconds(10));
+        wait = new WebDriverWait(driver, ofSeconds(10));
+    }
+
+    @Test
+    public void test(){
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
